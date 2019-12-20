322ba56bee7faffba82ee582cad9190eb53b3dfc
DavidJGChen <dchen1@andrew.cmu.edu>
2019-12-20 02:34:27 -0500

    Ship added, basic bullets added

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 direction;
+    public float moveSpeed = 40f;
+
+    private float timeToDeath = 2f;
+
+    private void FixedUpdate() {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        timeToDeath -= Time.deltaTime;
+        if (timeToDeath < 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    public Vector2 Direction {
+        get { return direction; }
+        set { 
+            direction = value; 
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dawid {
+public class Player : MonoBehaviour
+{
+    public GameObject bullet;
+    private Rigidbody2D rb2D;
+    private Vector2 velocity;
+    private Vector2 velocitySmoothing;
+    private float rotateAmount;
+    private float thrust;
+    public float moveSpeed = 8f;
+    public float rotateSpeed = 360f;
+    public float timeToAccel = 1f;
+    private void Awake() {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        rotateAmount = 0f;
+        thrust = 0f;
+    }
+
+    void Update() {
+        rotateAmount = Input.GetAxisRaw("Horizontal");
+        thrust = Input.GetAxisRaw("Vertical") == 1f ? 1f : 0f;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var thingy = Instantiate(bullet, transform.position + transform.up * 0.5f, new Quaternion());
+            thingy.GetComponent<Bullet>().Direction = transform.up;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0f, 0f, -1 * rotateAmount * rotateSpeed * Time.deltaTime);
+        Vector2 targetVelocity = transform.up * moveSpeed * thrust;
+        // Debug.Log(transform.up);
+        rb2D.velocity = Vector2.SmoothDamp(rb2D.velocity, targetVelocity, ref velocitySmoothing, timeToAccel);
+        // Debug.Log(rigidbody2D.velocity);
+    }
+}
+}
